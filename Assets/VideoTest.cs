7c2d8b35f9d4caa4a92f7148c5cf84e9f2d3be19
Keijiro Takahashi <keijiro@gmail.com>
2021-11-26 13:49:16 +0900

    Use temp RT

--- Assets/VideoTest.cs
+++ Assets/VideoTest.cs
@@ -14,27 +14,31 @@ sealed class VideoTest : MonoBehaviour
     float[] _tempArray = new float[4];
 
     Material _material;
-    RenderTexture _rt;
+    RenderTexture _rt1, _rt2;
 
     void Start()
     {
         _buffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 4, sizeof(float));
         _material = new Material(_shader);
-        _rt = new RenderTexture(8, 8, 0);
+        _rt1 = new RenderTexture(1920, 1080, 0);
+        _rt2 = new RenderTexture(8, 8, 0);
     }
 
     void OnDestroy()
     {
         _buffer.Dispose();
         Destroy(_material);
-        Destroy(_rt);
+        Destroy(_rt1);
+        Destroy(_rt2);
     }
 
     void Update()
     {
         if (_video.texture == null) return;
 
-        _compute.SetTexture(0, "Input", _video.texture);
+        Graphics.Blit(_video.texture, _rt1);
+
+        _compute.SetTexture(0, "Input", _rt1);
         _compute.SetBuffer(0, "Output", _buffer);
         _compute.Dispatch(0, 1, 1, 1);
 
@@ -42,9 +46,9 @@ sealed class VideoTest : MonoBehaviour
 
         _material.SetFloat("_Input", _tempArray[0]);
 
-        Graphics.Blit(null, _rt, _material);
+        Graphics.Blit(null, _rt2, _material);
 
-        _ui1.texture = _video.texture;
-        _ui2.texture = _rt;
+        _ui1.texture = _rt1;
+        _ui2.texture = _rt2;
     }
 }
